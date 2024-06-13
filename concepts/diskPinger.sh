4b4e8a2c806c07084360ce46bb92b7cff3f96737
ngrogg <oddiusoddone@gmail.com>
2024-06-13 00:21:22 +0000

    concepts/diskPinger.sh. Laid out structure comments. Removed more template code.

--- concepts/diskPinger.sh
+++ concepts/diskPinger.sh
@@ -4,7 +4,7 @@
 # BASH script to check disk space on a server and send an email
 # By Nicholas Grogg
 
-## Help function
+# Help function
 function helpFunction(){
 	printf "%s\n" \
 	"Help" \
@@ -14,27 +14,43 @@ function helpFunction(){
 	"* Display this help message and exit" \
 	" " \
 	"run/Run" \
-	"* PROGRAM DESCRIPTION " \
-	"* ARGUMENTS " \
-	"Ex. ./SCRIPT run ARG ARG"
+	"* Run the script and check for free disk space" \
+	"* Sends an email if free disk space less than 10%  " \
+    "* Takes a sender and recipent as arguments" \
+	"Ex. ./diskPinger.sh run sender recipient"
 }
 
-## Function to run program
+# Function to run program
 function runProgram(){
 	printf "%s\n" \
 	"RUN" \
 	"----------------------------------------------------"
+
+    ## Validation checks
+    ### Check if send blank
+    ### Check if recipient blank
+    ### Confirm values
+
+    ## Check available disk space
+    ### If less than 10% disk space free
+    ### Navigate to root, get list of files taking up space
+    cd /
+    du --max-depth=5 -chax  2>&1 | grep '[0-9\.]\+G' | sort -hr | head
+    ### Send email
+
+    ### Else exit 0
+
 }
 
-## Main, read passed flags
+# Main, read passed flags
 	printf "%s\n" \
-	"PROGRAM NAME" \
+	"Disk Pinger" \
 	"----------------------------------------------------" \
 	" " \
 	"Checking flags passed" \
 	"----------------------------------------------------"
 
-## Check passed flags
+# Check passed flags
 case "$1" in
 [Hh]elp)
 	printf "%s\n" \
@@ -47,7 +63,7 @@ case "$1" in
 	printf "%s\n" \
 	"Running script" \
 	"----------------------------------------------------"
-	runProgram
+	runProgram $2 $3
 	;;
 *)
 	printf "%s\n" \
